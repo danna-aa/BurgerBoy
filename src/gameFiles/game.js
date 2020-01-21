481cf5c542c4f104b8b21286d39ea344043a950b
danna-aa <danna@berkeley.edu>
2020-01-21 04:48:10 -0800

    Border

--- src/gameFiles/game.js
+++ src/gameFiles/game.js
@@ -31,9 +31,9 @@ class Game {
 
   randomizeY() {
     let posOrNeg;
-    if (this.placementY >= 211) {
+    if (this.placementY >= 216) {
       posOrNeg = -1;
-    } else if (this.placementY <= 210) {
+    } else if (this.placementY <= 215) {
       posOrNeg = 1;
     } else {
       posOrNeg = Math.random() < 0.5 ? -1 : 1;
